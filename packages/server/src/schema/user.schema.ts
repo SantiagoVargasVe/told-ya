b9cd6c894ac9dbe364154822d3dfc215de6725cf
Santiago Vargas <santiagovargasve@gmail.com>
2023-03-08 19:44:35 -0500

    ADD: Migration to delete user photo

--- packages/server/src/schema/user.schema.ts
+++ packages/server/src/schema/user.schema.ts
@@ -1,9 +1,11 @@
 import { object, string, TypeOf } from "zod";
 
 export const createUserSchema = object({
-  name: string({ required_error: "Name is required" }),
+  name: string({ required_error: "Name is required" }).min(
+    1,
+    "Name is required"
+  ),
   email: string({ required_error: "Email is required" }).email("Invalid email"),
-  photo: string({ required_error: "Photo is required" }),
   password: string({ required_error: "Password is required" })
     .min(8, "Password must be more than 8 characters")
     .max(32, "Password must be less than 32 characters"),
